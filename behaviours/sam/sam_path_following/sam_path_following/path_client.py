deafc910bfaf90ff3b3e924e48848bd4f1a3e130
David <ddorner@kth.se>
2026-03-10 16:46:23 +0100

    Different trajectory

--- behaviours/sam/sam_path_following/sam_path_following/path_client.py
+++ behaviours/sam/sam_path_following/sam_path_following/path_client.py
@@ -80,13 +80,11 @@ class PathClient(SMARCActionClient):
 
     def run(self):
         # DEBUGGING the trajectory tracking
-        #HERE = FilePath(__file__).parent  # resolves the directory of the script.
-        HERE = FilePath("/home/orin/colcon_ws/src/smarc2/behaviours/sam/sam_path_following/sam_path_following/")
-        # file_path = HERE / "trajectories" / "2026-01-19__straight_trajectory_1m.csv"
+        HERE = FilePath(__file__).parent  # resolves the directory of the script.
+        #HERE = FilePath("/home/orin/colcon_ws/src/smarc2/behaviours/sam/sam_path_following/sam_path_following/")
+        #file_path = HERE / "trajectories" / "2026-01-19__straight_trajectory_1m.csv"
         # file_path = HERE / "trajectories" / "turbo_turn_N11_alpha180_radius2.csv"
-        file_path = (
-            HERE / "trajectories" / "turbo_turn_zigzag_N20_alpha7_radius0.1.csv"
-        )
+        file_path = ( HERE / "trajectories" / "wp_x_4.5_y_0_z_0.csv")
 
         np_path = self.read_csv_to_array(file_path)
         mpc_trajectory = self.convert_np_path_to_trajectory(np_path)
